0f3986f45e46e3f3890e26142abb301afe4e781f
chazwilder <cwilder@chazwilder.onmicrosoft.com>
2024-10-15 15:34:27 -0400

    Tab Bars

--- src/main/frontend/app/(tabs)/_layout.jsx
+++ src/main/frontend/app/(tabs)/_layout.jsx
@@ -1,12 +1,100 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Tabs } from "expo-router";
+import React from "react";
+import { Image, View, Text } from "react-native";
+import { icons } from "../../constants";
+
+const TabIcon = ({ icon, name, color, focused }) => {
+  return (
+    <View className="items-center justify-center gap-2">
+      <Image 
+      source={icon}
+      resizeMode="contain"
+      tintColor={color}
+      className="w-8 h-8"
+
+      />
+      <Text className={`${focused ? 'font-psemibold' : 'font-pregular'} text-xs`}>
+        {name}
+      </Text>
+    </View>
+  );
+};
 
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
-  )
-}
+    <>
+      <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "white",
+          paddingVertical: 20,
+        },
+      }}
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                name="Home"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="food"
+          options={{
+            title: "Food",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.food}
+                color={color}
+                name="Food"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="protocols"
+          options={{
+            title: "Protocols",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.protocol}
+                color={color}
+                name="Protocols"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="community"
+          options={{
+            title: "Community",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.community}
+                color={color}
+                name="Community"
+                focused={focused}
+              />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
+  );
+};
 
-export default TabsLayout
+export default TabsLayout;
